2d920c5d3d5121f38c742f8795a27f1bd2a602a4
ddivito89 <daviddivito@Davids-MacBook-Pro.local>
2018-04-10 21:11:03 -0500

    removed usMongoClient true

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+  // useMongoClient: true
 });
+
 var ObjectId = require('mongodb').ObjectID
 
 app.get("/scrape", function(req, res) {
